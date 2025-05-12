c7fce3efed787a1452164e11405eb7f91e68c899
NiXon1x <125298533+NiXon1x@users.noreply.github.com>
2025-05-12 22:27:47 +0600

    Delete cineplex directory

--- cineplex/includes/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'session.php';
-require_once 'db.php';
-
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ../login.php");
-    exit();
-}
-?>
--- cineplex/includes/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Start session only if not already started
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-?>
